23902e7aaa14c7c83d9f3d6b8796aa80461ae59a
K3be <ahmed.13.alkaaby@gmail.com>
2019-12-05 20:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ class Polygon{
   constructor(arr){
     this.arr=arr;
   }
-}
 get countSides(){
   return this.arr.length;
 }
 get perimeter(){
   return this.arr.reduce((x,y) => x+y,0)
 }
-
+}
 class Triangle extends Polygon{
   get isValid(){
   if (this.arr.length !==3) return;
